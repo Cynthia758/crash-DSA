b1fdbec1b486e5fea3ff88317fefbc14b400176d
Cynthia <parmarvaidehi437@gmail.com>
2025-08-09 23:36:52 +0530

    Arithmatic-Operators

---
+++ operators.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main() {
+    int a,b;
+    cout << "Enter the value of a : ";
+    cin >> a;
+    cout << "Enter the value of b : ";
+    cin >> b;
+
+    cout << "The Summation of two numbers is : " << (a+b) << endl;
+    cout << "The Subtraction of two numbers is : " << (a-b) << endl;
+    cout << "The Multiplication of two numbers is : " << (a*b) << endl;
+    cout << "The Division of two numbers is : " << (a/b) << endl;
+    cout << "The Modulous of two numbers is : " << (a%b) << endl;
+
+
+    return 0;
+}
