68d7f4c978184ffeae0b875d393514347fc1610d
Cynthia <parmarvaidehi437@gmail.com>
2025-08-10 22:10:37 +0530

    Crash

--- operators.cpp
+++ operators.cpp
@@ -1,6 +1,7 @@
 #include<iostream>
 using namespace std;
 int main() {
+    // Arithmatic Operators
     int a,b;
     cout << "Enter the value of a : ";
     cin >> a;
@@ -12,6 +13,16 @@ int main() {
     cout << "The Multiplication of two numbers is : " << (a*b) << endl;
     cout << "The Division of two numbers is : " << (a/b) << endl;
     cout << "The Modulous of two numbers is : " << (a%b) << endl;
+
+    cout << (5/(double)2) << endl;
+
+    // Relational Operators
+
+    cout << (3<5) << endl;
+
+    cout << !(3<5) << endl;   // logical operators : NOT; and we are aware of the logical OR & AND.
+
+
 
 
     return 0;
